12914783a452db483c03a505d56b1c4524014e82
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-05-24 16:38:19 +0200

    refactor: handle animations in page element

--- src/views/page.js
+++ src/views/page.js
@@ -1,17 +1,51 @@
+function getAnimationDuration(element, prop = 'Animation') {
+    let computed = window.getComputedStyle(element);
+    if (computed) {
+        let duration = computed[`${prop.toLowerCase()}Duration`] ||
+            computed[`webkit${prop}Duration`] ||
+            computed[`moz${prop}Duration`] ||
+            computed[`ms${prop}Duration`] ||
+            computed[`o${prop}Duration`];
+        if (duration) {
+            return parseFloat(duration);
+        }
+    }
+    return 0;
+}
+
+function getTransitionDuration(element) {
+    return getAnimationDuration(element, 'Transition');
+}
+
 export class PageView {
     constructor() {
         let element = document.createElement('section');
+        element.className = 'page-view';
         Object.defineProperty(element, 'hide', {
-            value: () => {
-                element.style.display = 'none';
-                return Promise.resolve();
-            },
+            value: () =>
+                new Promise((resolve) => {
+                    element.setAttribute('hidden', '');
+                    element.setAttribute('data-hidden-animation', '');
+                    let duration = getAnimationDuration(element) ||
+                        getTransitionDuration(element) || 0;
+                    setTimeout(() => {
+                        element.removeAttribute('data-hidden-animation');
+                        resolve();
+                    }, duration * 1000);
+                }),
         });
         Object.defineProperty(element, 'show', {
-            value: () => {
-                element.style.display = 'block';
-                return Promise.resolve();
-            },
+            value: () =>
+                new Promise((resolve) => {
+                    element.removeAttribute('hidden');
+                    element.setAttribute('data-hidden-animation', '');
+                    let duration = getAnimationDuration(element) ||
+                        getTransitionDuration(element) || 0;
+                    setTimeout(() => {
+                        element.removeAttribute('data-hidden-animation');
+                        resolve();
+                    }, duration * 1000);
+                }),
         });
         return element;
     }
