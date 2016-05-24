8b1afe71c2675e2cee743ae97c9d5189f0400946
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-05-24 16:38:33 +0200

    refactor: improve ajax model

--- src/models/ajax.js
+++ src/models/ajax.js
@@ -7,19 +7,24 @@ export class AjaxModel extends Model {
     }
 
     fetch() {
-        return Ajax.get(this.endpoint).then((data) =>
-            Promise.resolve(data)
-        , (xhr) => {
-            // cordova status 0
-            if (xhr && xhr.status === 0 && typeof xhr.response !== 'undefined') {
-                return Promise.resolve(xhr.response);
-            }
-            return Promise.reject(xhr);
-        }).then((data) =>
-            this.transformInputData(data).then((props) => {
-                this.set(props);
-                return Promise.resolve(props);
-            })
-        );
+        if (this.endpoint) {
+            return Ajax.get(this.endpoint).then((data) =>
+                Promise.resolve(data)
+            , (xhr) => {
+                // cordova status 0
+                if (xhr && xhr.status === 0 &&
+                    typeof xhr.response !== 'undefined' &&
+                    xhr.response !== null) {
+                    return Promise.resolve(xhr.response);
+                }
+                return Promise.reject(xhr);
+            }).then((data) =>
+                this.transformInputData(data).then((props) => {
+                    this.set(props);
+                    return Promise.resolve(props);
+                })
+            );
+        }
+        return Promise.reject();
     }
 }
