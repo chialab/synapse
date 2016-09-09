c1f2ebf3e84185dac6f27e5c28faf57c45d65a74
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-09-09 09:30:26 +0200

    feat: handle injectors

--- src/mixins/injectable.js
+++ src/mixins/injectable.js
@@ -5,31 +5,49 @@ export const InjectableMixin = (superClass) => class extends superClass {
 
     initialize(...args) {
         super.initialize(...args);
-        let inject = this.constructor.inject;
+        if (this.constructor.injectors) {
+            this.registerInject(this.constructor.injectors);
+        }
+        if (this.constructor.inject) {
+            this.inject(this.constructor.inject);
+        }
+    }
+
+    registerInject(inject, Fn) {
+        if (Array.isArray(inject)) {
+            inject.forEach((name) => this.registerInject(name));
+        } else if (typeof inject === 'object') {
+            for (let name in inject) {
+                if (inject.hasOwnProperty(name)) {
+                    this.registerInject(name, inject[name]);
+                }
+            }
+        } else {
+            let owner = this;
+            if (typeof this.getOwner === 'function') {
+                owner = this.getOwner();
+            }
+            owner.injected = owner.injected || {};
+            owner.injected[inject] = new Fn(owner);
+        }
+    }
+
+    inject(inject) {
         if (Array.isArray(inject)) {
             inject.forEach((name) => this.inject(name));
         } else if (typeof inject === 'object') {
             for (let name in inject) {
                 if (inject.hasOwnProperty(name)) {
-                    this.inject(name, inject[name]);
                     this.inject(name);
                 }
             }
-        }
-    }
-
-    inject(name, Fn) {
-        let owner = this;
-        if (typeof this.getOwner === 'function') {
-            owner = this.getOwner();
-        }
-        this.injected = this.injected || {};
-        if (typeof Fn === 'undefined') {
-            this.injected[name] = owner.factory(name);
-        } else if (owner !== this) {
-            owner.inject(name, Fn);
         } else {
-            owner.injected[name] = new Fn(owner);
+            let owner = this;
+            if (typeof this.getOwner === 'function') {
+                owner = this.getOwner();
+            }
+            this.injected = this.injected || {};
+            this.injected[inject] = owner.factory(inject);
         }
     }
 
