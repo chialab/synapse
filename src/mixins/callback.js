92464e9ba26218751f87e0cf9e4fe7a945f07a29
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2016-11-09 19:08:49 +0100

    feat: add listen and unlisten methods to callback mixin

--- src/mixins/callback.js
+++ src/mixins/callback.js
@@ -1,10 +1,28 @@
 import { mix } from '../helpers/mixin.js';
+import { internal } from '../helpers/internal.js';
 import { CallbackManager } from 'chialab-callback-manager/src/callback-manager.js';
 
 export const CallbackMixin =
     (superClass) => class extends mix(superClass).with(CallbackManager.mixin) {
+        constructor(...args) {
+            super(...args);
+            internal(this).listeners = [];
+        }
+
+        listen(scope, ev, callback) {
+            internal(this).listeners.push(
+                scope.on(ev, callback)
+            );
+        }
+
+        unlisten() {
+            internal(this).listeners.forEach((destroy) => destroy());
+            internal(this).listeners = [];
+        }
+
         destroy() {
             super.destroy();
+            this.unlisten();
             this.off();
         }
     };
