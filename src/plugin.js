fbd91cf16f4be93f8eda18b893ec9446eada67e9
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2017-02-17 11:38:39 +0100

    fix: plugin initialization

--- src/plugin.js
+++ src/plugin.js
@@ -4,4 +4,16 @@ export class Plugin extends BaseObject {
     static get dependencies() {
         return [];
     }
+
+    initialize(owner, conf) {
+        return super.initialize(owner, conf)
+            .then(() => {
+                this.configure(conf);
+                return Promise.resolve();
+            });
+    }
+
+    configure() {
+        return true;
+    }
 }
