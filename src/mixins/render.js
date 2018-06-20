d08e195edb7aab12cb197674ae1e4348d1cfc7d5
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2018-06-20 17:56:35 +0200

    feat: add _render private method

--- src/mixins/render.js
+++ src/mixins/render.js
@@ -2,21 +2,48 @@ import { internal } from '../helpers/internal.js';
 import { DOM, IDOM } from '@dnajs/idom';
 
 export const RenderMixin = (SuperClass) => class extends SuperClass {
+    /**
+     * The root node element of the App.
+     * @type {Element}
+     */
     get root() {
         return DOM.getComponentNode(this.element) || this.element;
     }
 
+    /**
+     * @inheritdoc
+     */
     initialize(...args) {
         return super.initialize(...args)
-            .then(() => {
-                this._setRendering();
-                IDOM.patch(this.root, this.render());
-                this._unsetRendering();
-                return this._rendered();
-            });
+            .then(() => this._render());
     }
 
+    /**
+     * Update App rendering.
+     * @private
+     *
+     * @return {Promise}
+     */
+    _render() {
+        this._setRendering();
+        const content = this.render();
+        const root = this.root;
+        if (typeof content === 'string') {
+            root.innerHTML = content;
+        } else {
+            IDOM.patch(root, content);
+        }
+        this._unsetRendering();
+        return this._rendered();
+    }
+
+    /**
+     * The render template.
+     *
+     * @return {Function} The JSX template of the App.
+     */
     render() { }
+
     /**
      * Set the application in rendering mode.
      * @private
@@ -25,6 +52,7 @@ export const RenderMixin = (SuperClass) => class extends SuperClass {
         internal(this).rendering = true;
         internal(this).renderingPromises = [];
     }
+
     /**
      * Unset the application from rendering mode.
      * @private
@@ -32,6 +60,7 @@ export const RenderMixin = (SuperClass) => class extends SuperClass {
     _unsetRendering() {
         internal(this).rendering = false;
     }
+
     /**
      * Add a rendering promise.
      * @private
@@ -41,6 +70,7 @@ export const RenderMixin = (SuperClass) => class extends SuperClass {
     _addRendering(rendering) {
         internal(this).renderingPromises.push(rendering);
     }
+
     /**
      * Check if app is in rendering mode.
      * @private
@@ -50,6 +80,7 @@ export const RenderMixin = (SuperClass) => class extends SuperClass {
     _isRendering() {
         return !!internal(this).rendering;
     }
+
     /**
      * Return the rendering resolution queue.
      * @private
