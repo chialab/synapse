2d65145514ff7539b445c5de1a739a67e423a303
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2021-12-09 10:59:26 +0100

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+export type { RequestParams } from './Router/Request';
+export type { View, Meta } from './Router/Response';
+export type { RouteRule, NextHandler, RouteHandler } from './Router/Route';
+export type { MiddlewareRule, MiddlewareBeforeHandler, MiddlewareAfterHandler } from './Router/Middleware';
+export type { ErrorHandler, RouterOptions, PopStateData } from './Router/Router';
+
 export * from '@chialab/dna';
 export { Request } from './Router/Request';
 export { Response } from './Router/Response';
 export { Route } from './Router/Route';
+export { Middleware } from './Router/Middleware';
 export { History } from './Router/History';
 export { Router } from './Router/Router';
 export { App } from './App';
