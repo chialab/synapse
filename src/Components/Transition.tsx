bf6ae09167bfdf34319827fc3c438cc7c21dfdb4
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-05-24 12:47:11 +0200

    refactor: improve transition check

--- src/Components/Transition.tsx
+++ src/Components/Transition.tsx
@@ -1,95 +1,14 @@
-import type { Context, FunctionComponent, Template } from '@chialab/dna';
+import type { FunctionComponent, Template } from '@chialab/dna';
 import type { State } from '../Router/State';
 import type { Router } from '../Router/Router';
-
-/**
- * A callback for animation listeners.
- */
-type AnimationListener = (event: AnimationEvent) => void;
+import { window, Node } from '@chialab/dna';
 
 /**
  * Transition component properties.
  */
 type TransitionProps = {
     router: Router;
-    timeout?: number;
 };
-
-/**
- * Transition component render context.
- */
-type TransitionContext = Context<Node, TransitionProps>;
-
-/**
- * Check if the animation event target is in the render context.
- * @param event The animation event.
- * @param context The render context.
- * @returns True if the target is rendered by the context, false otherwise.
- */
-function isEventInContext(event: AnimationEvent, context: TransitionContext): boolean {
-    const target = event.target;
-    const { start, end } = context;
-
-    let check = start;
-    while (check && check !== end) {
-        check = check.nextSibling as Node;
-        if (check === target) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-/**
- * Create the animation start listener.
- * Trigger a re-render if transition start timed out.
- * @param context The render context.
- * @param timeout Transition start timeout.
- * @returns An animation listener callback.
- */
-function createStartListener(context: TransitionContext, timeout = 100): AnimationListener {
-    const { store, requestUpdate } = context;
-
-    const noAnimationTimeout = setTimeout(() => {
-        store.set('previousChildren', null);
-        requestUpdate?.();
-    }, timeout);
-
-    return function(event) {
-        if (!isEventInContext(event, context)) {
-            return;
-        }
-
-        clearTimeout(noAnimationTimeout);
-        const counter = (store.get('counter') as number || 0) + 1;
-        store.set('counter', counter);
-    };
-}
-
-/**
- * Create the animation end listener.
- * Trigger a re-render when page transition ends.
- * @param context The render context.
- * @returns An animation listener callback.
- */
-function createEndListener(context: TransitionContext): AnimationListener {
-    const { store, requestUpdate } = context;
-
-    return function(event) {
-        if (!isEventInContext(event, context)) {
-            return;
-        }
-
-        const counter = (store.get('counter') as number || 0) - 1;
-        store.set('counter', counter);
-
-        if (counter === 0) {
-            store.delete('previousChildren');
-            requestUpdate?.();
-        }
-    };
-}
 
 /**
  * Render responses with page transitions.
@@ -97,37 +16,67 @@ function createEndListener(context: TransitionContext): AnimationListener {
  * @param context The render context.
  * @returns A template of pages to animate.
  */
-export const Transition: FunctionComponent<TransitionProps> = function Transition({ router, children, timeout }, context) {
+export const Transition: FunctionComponent<TransitionProps> = function Transition({ router, children }, context) {
     if (!router) {
         throw new Error('Transition router is required');
     }
 
-    const { node, store } = context;
-
+    const { node, store, requestUpdate } = context;
     const currentState = store.get('state') as State | undefined;
-    const previousRoot = store.get('root') as HTMLElement|undefined;
     const root = node.parentElement;
-    if (root !== previousRoot) {
-        const listeners = (store.get('listeners') ?? [
-            createStartListener(context, timeout),
-            createEndListener(context),
-        ]) as [AnimationListener, AnimationListener];
-
-        if (previousRoot) {
-            previousRoot.removeEventListener('animationstart', listeners[0]);
-            previousRoot.removeEventListener('animationend', listeners[1]);
-        }
-        if (root) {
-            root.addEventListener('animationstart', listeners[0]);
-            root.addEventListener('animationend', listeners[1]);
-        }
-
-        store.set('root', root);
-        store.set('listeners', listeners);
-    }
 
     let previousChildren = store.get('previousChildren') as Template[] | undefined;
     if (currentState !== router.state) {
+        if (root) {
+            const start = function(event) {
+                if (event.target.parentNode !== root) {
+                    return;
+                }
+
+                const counter = (store.get('counter') as number || 0) + 1;
+                store.set('counter', counter);
+            };
+
+            const end = function(event) {
+                if (event.target.parentNode !== root) {
+                    return;
+                }
+
+                const counter = (store.get('counter') as number || 0) - 1;
+                store.set('counter', counter);
+
+                if (counter === 0) {
+                    flush();
+                }
+            };
+
+            const flush = function() {
+                root.removeEventListener('animationstart', start);
+                root.removeEventListener('animationend', end);
+                store.delete('previousChildren');
+                requestUpdate?.();
+            };
+
+            root.addEventListener('animationstart', start);
+            root.addEventListener('animationend', end);
+            setTimeout(() => {
+                let node = context.start as Node;
+                while (node && node !== context.end) {
+                    node = node.nextSibling as Node;
+                    if (node.nodeType === Node.ELEMENT_NODE) {
+                        const style = window.getComputedStyle(node as HTMLElement);
+                        const animation = style.getPropertyValue('animation-name');
+                        const duration = style.getPropertyValue('animation-duration');
+                        if (animation !== 'none' && duration !== '0s') {
+                            return;
+                        }
+                    }
+                }
+
+                flush();
+            });
+        }
+
         previousChildren = store.get('children') as Template[] | undefined;
         store.set('counter', 0);
         store.set('previousChildren', previousChildren);
