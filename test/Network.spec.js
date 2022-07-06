94206c7214966bf7bdc7d0c71745223e75c97d28
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-07-06 10:17:02 +0200

    test: add browser test job

--- test/Network.spec.js
+++ test/Network.spec.js
@@ -1,11 +1,12 @@
 import { expect } from '@chialab/ginsenghino';
+import { window } from '@chialab/dna';
 import { fetch } from '@chialab/synapse';
 
 describe('Network', () => {
     it('should fetch content from url', async () => {
         expect(fetch).to.be.a('function');
 
-        const response = await fetch('http://www.example.com');
+        const response = await fetch(window.location.origin);
         expect(response).to.not.be.null;
         expect(typeof response).to.be.equal('object');
     });
