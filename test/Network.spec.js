468b67bcd09e23d74cb730359ca28d5b41a06fe3
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-07-06 10:23:22 +0200

    test: fix fetch test

--- test/Network.spec.js
+++ test/Network.spec.js
@@ -6,7 +6,9 @@ describe('Network', () => {
     it('should fetch content from url', async () => {
         expect(fetch).to.be.a('function');
 
-        const response = await fetch(window.location.origin);
+        const response = await fetch('https://www.chialab.it', {
+            mode: 'no-cors',
+        });
         expect(response).to.not.be.null;
         expect(typeof response).to.be.equal('object');
     });
