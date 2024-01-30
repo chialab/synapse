281c0d8d0a4774f8f53e1e96f55523708bfe88b2
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2024-01-30 18:22:21 +0100

    Rename scripts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: 'synapse',
+    base: '/synapse',
     build: {
         outDir: 'public',
     },
